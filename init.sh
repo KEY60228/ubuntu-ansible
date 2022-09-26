d79e0a875accf78e49c7db67978249cbbba9d1aa
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-09-26 18:48:20 +0900

    homebrew管理からapt管理に変更

--- init.sh
+++ init.sh
@@ -5,18 +5,8 @@ ANSIBLE_PATH=~/ansible
 ## apt update
 apt update && apt upgrade -y
 
-## git他必要なものとhomebrewのinstall
-apt-get install build-essential procps curl file git
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-## homebrewのパスを通す
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
-
-## ansibleのinstall
-brew install ansible
+## 最低限必要なものとansibleのinstall
+apt-get install build-essential procps curl file git ansible
 
 ## repositoryのclone
 git clone https://github.com/KEY60228/ubuntu-ansible.git ${ANSIBLE_PATH}
