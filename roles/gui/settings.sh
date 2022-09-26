a2d985bd1e5bf1db601617701ed1463c7581ef02
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-09-26 10:38:55 +0900

    add settings.sh

---
+++ roles/gui/settings.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## Top Bar
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+## Screen Lock
+gsettings set org.gnome.desktop.session idle-delay $((15*60))
+gsettings set org.gnome.desktop.screensaver lock-delay $((15*60))
+
+## Keyboards
+sudo apt-get update && sudo apt-get install $(check-language-support -l ja)
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'mozc-jp')]"
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
+## Dock
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BUTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
+
+## Power
+gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
